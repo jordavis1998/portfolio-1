8076bf06e85ca338c00b08214671b41a31df0095
Jordan Davis <97630857+jordavis1998@users.noreply.github.com>
2022-03-04 16:04:57 -0500

    March 4, 2022 4:04 PM

--- index.php
+++ index.php
@@ -4,20 +4,9 @@
       <p>Please share your thoughts!</p>
   </head>
 <?php
-// Update log from POST
-// function updateFromPOST() {
-    // Does the value exist?
+    // post of the value in textarea
     if(isset($_POST["submit"])) {
       echo $_POST["log"];
-
-    // // Get the current contents
-    // $currentContent = readLog();
-    
-    // // Add a <br> between content
-    // $currentContent = $currentContent . "<br>" . $_POST["log"];
-    
-    // // Update the file
-    // updateLog($currentContent);
     }
   
 ?>
