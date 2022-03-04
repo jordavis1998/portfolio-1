473aff4dd561d048ed606d1582e69ffcbcb37e76
Jordan Davis <97630857+jordavis1998@users.noreply.github.com>
2022-03-04 14:18:40 -0500

    updates

---
+++ index.php
@@ -0,0 +1,17 @@
+<?php
+// Update log from POST
+function updateFromPOST() {
+    // Does the value exist?
+    if(isset($_POST["log"])) {
+
+    // Get the current contents
+    $currentContent = readLog();
+    
+    // Add a <br> between content
+    $currentContent = $currentContent . "<br>" . $_POST["log"];
+    
+    // Update the file
+    updateLog($currentContent);
+    }
+  }
+?>
